d98bc0ced8145e1e6b56a8928445f6cc8873ea61
FarzanVM <farzanmohan@gmail.com>
2024-03-21 22:56:43 +0530

    product card-70%

--- src/app/homepage/product-card/product-card.component.ts
+++ src/app/homepage/product-card/product-card.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHeart,faStar} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss'
 })
 export class ProductCardComponent {
+  faHeart=faHeart;
+  faStar=faStar;
 
 }
