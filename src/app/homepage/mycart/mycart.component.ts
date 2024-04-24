f02c6394ee12f33e644d852dbd4349011286f109
Farzan V V M <farzan.61093361@ltimindtree.com>
2024-04-24 18:12:55 +0530

    cart design progress 95%

--- src/app/homepage/mycart/mycart.component.ts
+++ src/app/homepage/mycart/mycart.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faIndianRupee,faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-mycart',
   standalone: true,
-  imports: [],
+  imports:[FontAwesomeModule],
   templateUrl: './mycart.component.html',
   styleUrl: './mycart.component.scss'
 })
 export class MycartComponent {
+  farupee=faIndianRupee
+  faStar=faStar
 
 }
