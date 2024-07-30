560b118ac9fcc78393016456f3ab527141c2e597
Farzan V V M <farzan.61093361@ltimindtree.com>
2024-07-30 20:08:01 +0530

    order functionality changes added

--- src/app/homepage/cartitemcard/cartitemcard.component.ts
+++ src/app/homepage/cartitemcard/cartitemcard.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterContentInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faIndianRupee, faMinus, faPlus, faStar, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { CartService } from '../../services/cartservice/cart.service';
@@ -12,7 +12,7 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './cartitemcard.component.html',
   styleUrl: './cartitemcard.component.scss'
 })
-export class CartitemcardComponent implements OnInit {
+export class CartitemcardComponent implements OnInit ,AfterContentInit{
 
   @Input() item:any;
   @Output() selectedItem=new EventEmitter<Object>();
@@ -25,12 +25,23 @@ export class CartitemcardComponent implements OnInit {
   faminus=faMinus;
 
   quantity:number=1;
-  isChecked:boolean=false;
+  isChecked:boolean=true;
 
   constructor(private cartservice:CartService){}
+ 
 
   ngOnInit(): void {
-  
+  }
+
+  ngAfterContentInit(): void {
+    const product ={
+      product_id:this.item.products._id,
+      quantity:this.item.products.productquantity,
+      storename:this.item.products.storename,
+    }
+    setTimeout(()=>{
+      this.selectedItem.emit(product)
+    })
   }
   updateQuantity(product_id:any,count:any){
     this.quantity+=count
