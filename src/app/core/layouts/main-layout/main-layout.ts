333411f779cb06f1eb938c1166fe3b4a5ce4b5da
FarzanVM <farzanmohan@gmail.com>
2025-11-15 22:42:49 +0530

    refactor:create Footer Component and move all logic

--- src/app/core/layouts/main-layout/main-layout.ts
+++ src/app/core/layouts/main-layout/main-layout.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Header } from "../../../shared/components/header/header";
+import { Footer } from "../../../shared/components/footer/footer";
+import { AdminRoutingModule } from "../../../pages/admin/admin-routing.module";
 
 @Component({
   selector: 'app-main-layout',
   standalone:true,
-  imports: [Header],
+  imports: [Header, Footer, AdminRoutingModule],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
